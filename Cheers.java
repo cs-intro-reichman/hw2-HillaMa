a966caa63eb25e4f41dedaa372b6447421bf946a
הילה מעברי <hila.mabari@post.runi.ac.il>
2024-11-14 17:23:57 +0200

    Done

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,23 @@
 // Prints a crowd cheering output.
 public class Cheers {
-        public static void main(String[] args) {
-	    // Replace this comment with your code
+        public static void main(String[] args) {   
+            String name = args[0];
+            int cheerTime = Integer.parseInt(args[1]);
+            String addAn = "aefhilmnorsxAEFHILMNORSX";
+            int i = 0;
+            while(i < name.length()) {
+                if(addAn.indexOf(name.charAt(i)) == -1) {
+                        System.out.println("Give me a  " + name.charAt(i) + ": " + name.charAt(i) + "!");
+                }
+                else {
+                        System.out.println("Give me an " + name.charAt(i) + ": " + name.charAt(i) + "!");
+                }
+                i++;
+            }
+            System.err.println("What does that spell?");
+            while(cheerTime > 0) {
+                System.err.println(name + "!!!");
+                cheerTime--;
+            }
         }
 }
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,26 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        String time = args[0];
+        int minutesToAdd = Integer.parseInt(args[1]);
+        int hours = Integer.parseInt(time.substring(0,2));
+        int minutes = Integer.parseInt(time.substring(3,5));
+        minutes = (hours * 60) + minutes +  minutesToAdd;
+        hours = minutes / 60;
+        minutes = minutes - (hours * 60);
+        hours = hours % 24;
+        if(hours / 10 == 0) {
+            if(minutes / 10 == 0){
+                System.out.println("0" + hours + ":0" +minutes);
+            }
+            else{
+                System.out.println("0" + hours + ":" +minutes);
+            }
+        }
+        else if(minutes / 10 == 0) {
+            System.out.println(hours + ":0" +minutes);
+        }
+        else {
+            System.out.println(hours + ":" +minutes);
+        }
     }
 }
